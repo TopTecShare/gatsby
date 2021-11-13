144e6e4a1c8bbcb798f94849a9ac48a84ccb4cac
TopTecShare <eestimorol.ice9208g@gmail.com>
2021-11-14 00:03:45 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "EviStamp",
+    title: "My First Gatsby Site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
